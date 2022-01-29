6e5c93b6225ac1be30fb74e78cf8d49bf287b7e6
unknown <sergeyandmelnikov@gmail.com>
2022-01-29 22:31:56 +0300

    save results (fix point)

--- src/index.php
+++ src/index.php
@@ -2,11 +2,55 @@
 
 define('ROOT', $_SERVER['DOCUMENT_ROOT']);
 
+include "debug.php";
+
 include ROOT . "/orm/orm.php";
 include ROOT . "/setup/config.php";
 
+class Books extends ORM {
+	public $id;
+	public $title;
+	public $original_title;
+	public $author_id;
+	public $year;
+	public $annotation;
+	public $ISBN;
+	public $rating;
+}
+
+class Genres extends ORM {
+	public $id;
+	public $title;
+}
+
+class Authors extends ORM {
+	public $id;
+	public $full_name; 
+}
+
 try {
 	orm::setup(new mysqli($db_host, $db_user, $db_pass, 'books')) or die(orm::getError());
+
+	$book = books::findID(1);
+	
+	echo "<pre>";
+	print_r($book);
+	echo "</pre>";
+	echo __FILE__ . ': ' . __LINE__;
+
+	$genre = genres::findID(8);
+
+	echo "<pre>";
+	print_r($genre);
+	echo "</pre>";
+	echo __FILE__ . ': ' . __LINE__;
+
+	$author = authors::findID(1);
+
+	echo "<pre>";
+	print_r($author);
+	echo "</pre>";
+	echo __FILE__ . ': ' . __LINE__;
 } 
 catch(Exception $e) {
 	echo "В ходе работы возникла ошибка";
--- src/orm/orm.php
+++ src/orm/orm.php
@@ -20,6 +20,41 @@ class ORM {
 		return self::$db->query("SET NAMES `$enc`");
 	}
 
+	static function query($query) 
+	{
+		return self::$db->query($query);
+	}
+
+	static function findID($id) 
+	{
+		if(is_numeric($id)){
+			$query = "SELECT * FROM `" . get_called_class() . "`WHERE`" . key(get_class_vars(get_called_class())) . "` = $id LIMIT 1";
+			$result = self::$db->query($query);
+
+			if($result->num_rows == 1) {
+				// Возвращаем экземпляр
+				$row = $result->fetch_object();
+
+				$className = get_called_class();
+
+				$returnClass = new $className();
+
+				foreach($row as $key => $value) {
+					$returnClass->$key = $value;
+				}
+
+				return $returnClass;
+
+			} else {
+				return false;
+			}
+
+		} else {
+			throw new Exception("Параметр $id содержит данные другого типа", 1);
+			return false;
+		}
+	}
+
 	static function getError() 
 	{
 		return self::$db->error . "[". self::$db->errno ."]";
